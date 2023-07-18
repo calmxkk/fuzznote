c9881d3f65fed507e4aee46215f9314a2816ffbb
calmxkk <zhaokai417@163.com>
2023-07-18 03:19:43 +0000

    add peach

---
+++ peach/pits/dns/monitor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-"
+
+import dns.resolver
+import time
+
+myResolver = dns.resolver.Resolver()
+myResolver.nameservers = ['172.17.0.5']
+myResolver.port = 53
+filename = "./monitor-log/peach-dns-log"
+
+try:
+    myAnswers = myResolver.resolve("calmxkk.com", lifetime=2)
+except:
+    with open(filename,'a')as file:
+        file.write(time.strftime("%Y-%m-%d %X", time.localtime()) + " " + myResolver.nameservers + " dns error\n")
+    print("error")
+else:
+    print("success")
